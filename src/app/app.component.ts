1ad70b439be60626b308012cb376ec678578184e
Paul Norling <paulnorling87@gmail.com>
2024-04-30 11:29:47 -0500

    remove errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,45 +1,40 @@
-import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
-import { Hero } from '../hero'
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-import {
-  NgIf,
-  NgFor,
-  /* . . . */
-} from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [
-    FormsModule,
-    NgIf,
-    NgFor,
-    UpperCasePipe,
-    HeroDetailComponent
-  ],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
-  
-  heroes: Hero[] = [];
 
-  selectedHero!: Hero;
-
-  constructor(private heroService: HeroService) {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
