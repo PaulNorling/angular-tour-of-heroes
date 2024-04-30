1903a745b54e2f31c2716366586cb9a15277b67d
Paul Norling <paulnorling87@gmail.com>
2024-04-30 11:47:59 -0500

    remove duplicate app.module file

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,10 +11,3 @@ export class HeroDetailComponent {
   @Input() hero: Hero | undefined;
 
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
